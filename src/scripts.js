be73de03594c34ebfbaaaa9d825e7674ae3e2a4e
Filipe Alves <63798776+filipealvess@users.noreply.github.com>
2020-08-26 14:53:56 -0300

    Adição do LocalStorage

--- src/scripts.js
+++ src/scripts.js
@@ -2,12 +2,111 @@ const startScreen = document.querySelector('.startScreen');
 const inputPointsTotal = document.querySelector('.pointsTotal');
 const btnStart = document.querySelector('.btnStart');
 const mainContent = document.querySelector('.content');
+const btnRedo = document.querySelector('.redo');
+const confirmationScreen = document.querySelector('.confirmation');
+const btnConfirm = document.querySelector('.confirm');
+const btnCancel = document.querySelector('.cancel');
 const usedPointsQuantity = document.querySelector('.count');
 const totalPointsQuantity = document.querySelector('.total');
 const items = document.querySelector('.items');
 const btnAddItem = document.querySelector('.btnAddItem');
 
-const showErrorMessage = (input, message, timeInMilliseconds=3000) => {
+const ItemsInfo = {
+  get() {
+    return JSON.parse(localStorage.getItem('itemsInfo'));
+  },
+  set(info) {
+    localStorage.setItem('itemsInfo', JSON.stringify(info));
+  },
+  remove() {
+    localStorage.removeItem('itemsInfo');
+  }
+};
+
+const PointsQuantity = {
+  get() {
+    return JSON.parse(localStorage.getItem('pointsQuantity'));
+  },
+  set(points) {
+    localStorage.setItem('pointsQuantity', JSON.stringify(points));
+  },
+  remove() {
+    localStorage.removeItem('pointsQuantity');
+  }
+};
+
+if (PointsQuantity.get()) {
+  usedPointsQuantity.innerText = PointsQuantity.get().used;
+  totalPointsQuantity.innerText = PointsQuantity.get().total;
+
+  if (ItemsInfo.get()) {
+    for (itemInfo of ItemsInfo.get()) {
+      const item = `
+        <div class="item">
+          <button type="button" class="remove" onclick="removeItem(this)">×</button>
+          <div class="errorBlock">
+            <input
+              type="text"
+              placeholder="Título do item"
+              class="title"
+              value="${itemInfo.title}"
+              oninput="ItemsInfo.set(getItemsPresentInDOM())"
+            >
+            <p class="error">Erro</p>
+          </div>
+          <button type="button" class="minus" onclick="decrement(this)">-</button>
+          <div class="errorBlock">
+            <input
+              class="value"
+              type="number"
+              value="${itemInfo.points}"
+              min="0"
+              onkeydown="updateItemPointsIfKeyAllowed(event)"
+              oninput="handlePointsUpdate(this)"
+            >
+            <p class="error">Erro</p>
+          </div>
+          <button type="button" class="plus" onclick="increment(this)">+</button>
+        </div>
+      `;
+
+      items.insertAdjacentHTML('beforeend', item);
+    }
+  }
+
+  removeClass(startScreen, 'active');
+  addClass(mainContent, 'active');
+}
+
+function getPointsQuantity() {
+  return {
+    used: usedPointsQuantity.innerText,
+    total: totalPointsQuantity.innerText
+  };
+}
+
+function getItemsPresentInDOM() {
+  const itemsPresentInDOM = [...items.children];
+
+  return itemsPresentInDOM.reduce((itemsPresent, currentItem) => {
+    const title = currentItem.querySelector('.title').value;
+    const points = currentItem.querySelector('.value').value;
+
+    itemsPresent.push({title: title, points: points});
+
+    return itemsPresent;
+  }, []);
+}
+
+function addClass(element, elementClass) {
+  element.classList.add(elementClass);
+};
+
+function removeClass(element, elementClass) {
+  element.classList.remove(elementClass);
+};
+
+function showErrorMessage(input, message) {
   const error = input.nextElementSibling;
 
   error.innerText = message;
@@ -20,21 +119,21 @@ const showErrorMessage = (input, message, timeInMilliseconds=3000) => {
     setTimeout(() => {
       error.style.visibility = 'hidden';
     }, 500);
-  }, timeInMilliseconds);
+  }, 3000);
 };
 
-const preventEmpty = input => {
+function preventEmpty(input) {
   if (input.value.length === 0) {
     input.value = 0;
   }
 };
 
-const removeLastNumber = input => {
+function removeLastNumber(input) {
   const value = input.value;
   input.value = value.substring(0, value.length - 1);
 };
 
-const handlePointsUpdate = input => {
+function handlePointsUpdate(input) {
   const itemsPoints = [...items.querySelectorAll('.value')];
 
   const totalItemsPoints = itemsPoints.reduce((total, {value}) => {
@@ -45,16 +144,18 @@ const handlePointsUpdate = input => {
     usedPointsQuantity.innerText = totalItemsPoints;
   } else {
     const message = 'Valor maior que o total';
-    const timeInMilliseconds = 2000;
 
-    showErrorMessage(input, message, timeInMilliseconds);
+    showErrorMessage(input, message);
     removeLastNumber(input);
   }
 
   preventEmpty(input);
+
+  PointsQuantity.set(getPointsQuantity());
+  ItemsInfo.set(getItemsPresentInDOM());
 };
 
-const updateItemPointsIfKeyAllowed = event => {
+function updateItemPointsIfKeyAllowed(event) {
   const { target, key } = event;
   const allowedKeys = [
     'backspace',
@@ -81,14 +182,12 @@ const updateItemPointsIfKeyAllowed = event => {
   if (isAllowedKey) return;
 
   const message = 'Digite apenas números';
-  const timeInMilliseconds = 2000;
-
-  showErrorMessage(target, message, timeInMilliseconds);
+  showErrorMessage(target, message);
 
   event.preventDefault();
 }
 
-const isNumber = ([...values]) => {
+function isNumber([...values]) {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let isNumber = true;
 
@@ -107,31 +206,37 @@ const isNumber = ([...values]) => {
   return isNumber;
 };
 
-const increment = btnPlus => {
+function increment(btnPlus) {
   const itemPoints = btnPlus.parentNode.querySelector('.value');
 
   if (+usedPointsQuantity.innerText < +totalPointsQuantity.innerText) {
     itemPoints.value++;
     usedPointsQuantity.innerText++;
+
+    PointsQuantity.set(getPointsQuantity());
+    ItemsInfo.set(getItemsPresentInDOM());
   } else {
     const message = 'Chegou no valor máximo';
     showErrorMessage(itemPoints, message);
   }
 };
 
-const decrement = btnMinus => {
+function decrement(btnMinus) {
   const itemPoints = btnMinus.parentNode.querySelector('.value');
 
   if (+itemPoints.value > 0) {
     itemPoints.value--;
     usedPointsQuantity.innerText--;
+
+    PointsQuantity.set(getPointsQuantity());
+    ItemsInfo.set(getItemsPresentInDOM());
   } else {
     const message = 'Chegou no valor mínimo';
     showErrorMessage(itemPoints, message);
   }
 };
 
-const addNemItem = () => {
+function addNemItem() {
   const lastItem = items.lastElementChild;
   const lastItemTitle = lastItem
     ? lastItem.querySelector('.title').value
@@ -142,7 +247,12 @@ const addNemItem = () => {
       <div class="item">
         <button type="button" class="remove" onclick="removeItem(this)">×</button>
         <div class="errorBlock">
-          <input type="text" placeholder="Título do item" class="title">
+          <input
+            type="text"
+            placeholder="Título do item"
+            class="title"
+            oninput="ItemsInfo.set(getItemsPresentInDOM())"
+          >
           <p class="error">Erro</p>
         </div>
         <button type="button" class="minus" onclick="decrement(this)">-</button>
@@ -163,16 +273,17 @@ const addNemItem = () => {
 
     items.insertAdjacentHTML('beforeend', newItem);
     items.lastElementChild.querySelector('.title').focus();
+
+    ItemsInfo.set(getItemsPresentInDOM());
   } else {
     const message = 'Preencha antes de continuar';
     const input = items.lastElementChild.querySelector('.title');
-    const timeInMilliseconds = 2000;
 
-    showErrorMessage(input, message, timeInMilliseconds);
+    showErrorMessage(input, message);
   }
 };
 
-const removeItem = btnRemoveItem => {
+function removeItem (btnRemoveItem) {
   const item = btnRemoveItem.parentNode;
   const itemPoints = item.querySelector('.value');
 
@@ -181,24 +292,59 @@ const removeItem = btnRemoveItem => {
   }
 
   items.removeChild(item);
+
+  PointsQuantity.set(getPointsQuantity());
+  ItemsInfo.set(getItemsPresentInDOM());
 };
 
-const init = () => {
+function redo() {
+  removeClass(confirmationScreen, 'active');
+  removeClass(mainContent, 'active');
+  addClass(startScreen, 'active');
+
+  items.innerHTML = '';
+  usedPointsQuantity.innerText = '0';
+
+  ItemsInfo.remove();
+  PointsQuantity.remove();
+
+  inputPointsTotal.focus();
+};
+
+function cancel() {
+  removeClass(confirmationScreen, 'active')
+}
+
+function openConfirmation() {
+  addClass(confirmationScreen, 'active')
+}
+
+function initIfPressEnter({ key }) {
+  !(key.toLowerCase() === 'enter') || init()
+}
+
+function init() {
   const value = inputPointsTotal.value;
 
   if (value.length && isNumber(value)) {
     totalPointsQuantity.innerText = value;
 
-    startScreen.classList.remove('active');
-    mainContent.classList.add('active');
+    removeClass(startScreen, 'active');
+    addClass(mainContent, 'active');
+
+    PointsQuantity.set({used: 0, total: value});
+
+    inputPointsTotal.blur();
+    inputPointsTotal.value = '';
   } else {
     const message = 'O valor deve ser inteiro e maior que 0';
     showErrorMessage(inputPointsTotal, message);
   }
 };
 
-inputPointsTotal.addEventListener('keydown', ({ key }) => {
-  !(key.toLowerCase() === 'enter') || init();
-});
+inputPointsTotal.addEventListener('keydown', initIfPressEnter);
+btnRedo.addEventListener('click', openConfirmation);
+btnConfirm.addEventListener('click', redo);
+btnCancel.addEventListener('click', cancel);
 btnStart.addEventListener('click', init);
 btnAddItem.addEventListener('click', addNemItem);
